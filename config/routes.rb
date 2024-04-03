8ff987c817d22d01396e622bd5bfdcc0094756a1
area6080 <161906317+area6080@users.noreply.github.com>
2024-04-03 04:03:15 +0000

    [Start]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
   devise_for :users
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homes#top'
-  get 'homes/about' => 'homes#about' 
+  get 'homes/about' => 'homes#about' ,as: "about"
+  resources :post_images, only: [:new, :index, :show]
+
 end
