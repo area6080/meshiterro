2d50ef2bc869b84d6892be16098913e292c108f1
area6080 <161906317+area6080@users.noreply.github.com>
2024-04-03 08:09:46 +0000

    [Start]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
-  # get 'post_images/new'
-  # get 'post_images/index'
-  # get 'post_images/show'
   devise_for :users
   root 'homes#top'
   get 'homes/about' => 'homes#about' ,as: "about"
   resources :post_images, only: [:new, :create,:index, :show, :destroy]
-
+  resources :users, only: [:show, :edit]
+ 
 end
+
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
